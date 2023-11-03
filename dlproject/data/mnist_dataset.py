34e0a2d55397335be074e2103e194afa0086c482
Federico Calandra <94709756+FedericoCalandra@users.noreply.github.com>
2023-11-03 12:01:52 +0100

    Update mnist_dataset.py

--- dlproject/data/mnist_dataset.py
+++ dlproject/data/mnist_dataset.py
@@ -13,13 +13,13 @@ class MNISTDatasetBuilder:
     def __init__(self):
         (self.__train_x, self.__train_y), (self.__test_x, self.__test_y) = tf.keras.datasets.mnist.load_data()
         
-    def preprocess_dataset_simple_ae(self):
+    def preprocess_dataset_simple_ae(self, noise_factor: float = 0.4):
         self.__train_x = self.__preprocess_array(self.__train_x)
         self.__test_x = self.__preprocess_array(self.__test_x)
-        self.__noisy_train_data = self.__add_noise(self.__train_x)
-        self.__noisy_test_data = self.__add_noise(self.__test_x)
+        self.__noisy_train_data = self.__add_noise(self.__train_x, noise_factor)
+        self.__noisy_test_data = self.__add_noise(self.__test_x, noise_factor)
 
-    def preprocess_dataset_simple_vae(self):
+    def preprocess_dataset_simple_vae(self, noise_factor: float = 0.4):
         self.__train_x = self.__train_x.astype(np.float32) / 255.
         self.__test_x = self.__test_x.astype(np.float32) / 255.
         self.__train_x = self.__train_x.reshape((len(self.__train_x), np.prod(self.__train_x.shape[1:])))
@@ -31,7 +31,7 @@ class MNISTDatasetBuilder:
         return np.reshape(array, (len(array), 28, 28, 1))
 
     @staticmethod
-    def __add_noise(array: np.ndarray, noise_factor: float = 0.4) -> np.ndarray:
+    def __add_noise(array: np.ndarray, noise_factor) -> np.ndarray:
         noisy_array = array + noise_factor * np.random.normal(
             loc=0.0, scale=1.0, size=array.shape
         )
