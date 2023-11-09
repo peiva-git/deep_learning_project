454e6e773291ac7ed8a39a60e6346ca9ecc62619
federico_calandra <federico12597@gmial.com>
2023-11-09 11:41:30 +0100

    Added compute_psnr_ssim_metrics_from_directories() function in evaluation.py

--- dlproject/evaluation.py
+++ dlproject/evaluation.py
@@ -1,4 +1,6 @@
-from skimage.metrics import peak_signal_noise_ratio
+import os
+import cv2
+from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import structural_similarity as ssim
 
 import numpy as np
@@ -10,7 +12,7 @@ def mean_psnr(test_data, reconstructed_test_data):
 
     psnr_values = []
     for original, reconstructed in zip(test_data, reconstructed_test_data):
-        psnr_values.append(peak_signal_noise_ratio(original, reconstructed, data_range=255))
+        psnr_values.append(psnr(original, reconstructed, data_range=255.0))
     return np.mean(psnr_values)
 
 
@@ -22,3 +24,28 @@ def mean_ssim(test_data, reconstructed_test_data):
     for original, reconstructed in zip(test_data, reconstructed_test_data):
         ssim_values.append(ssim(original, reconstructed, data_range=255.0))
     return np.mean(ssim_values)
+
+
+def compute_psnr_ssim_metrics_from_directories(noisy_dir, restored_dir):
+    noisy_images = os.listdir(noisy_dir)
+    restored_images = os.listdir(restored_dir)
+
+    num_pairs = min(len(noisy_images), len(restored_images))
+
+    ssim_value = 0
+    psnr_value = 0
+
+    for i in range(num_pairs):
+        noisy_image_path = os.path.join(noisy_dir, noisy_images[i])
+        restored_image_path = os.path.join(restored_dir, restored_images[i])
+
+        noisy_image = cv2.imread(noisy_image_path)
+        restored_image = cv2.imread(restored_image_path)
+
+        psnr_value += psnr(restored_image, noisy_image)
+        ssim_value += ssim(restored_image, noisy_image, channel_axis=2)
+
+    mean_computed_psnr = psnr_value / num_pairs
+    mean_computed_ssim = ssim_value / num_pairs
+
+    return mean_computed_psnr, mean_computed_ssim
