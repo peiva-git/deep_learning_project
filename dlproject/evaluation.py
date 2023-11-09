0af523be2b46a239395aa1c3c0ed1fa664f2ddb9
federico_calandra <federico12597@gmial.com>
2023-11-09 16:05:15 +0100

    Bug fix

--- dlproject/evaluation.py
+++ dlproject/evaluation.py
@@ -26,26 +26,33 @@ def mean_ssim(test_data, reconstructed_test_data):
     return np.mean(ssim_values)
 
 
-def compute_psnr_ssim_metrics_from_directories(noisy_dir, restored_dir):
-    noisy_images = os.listdir(noisy_dir)
-    restored_images = os.listdir(restored_dir)
+def compute_psnr_ssim_metrics_from_directories(original_dir, restored_dir):
+    original_images = sort_paths_by_index(os.listdir(original_dir))
+    restored_images = sort_paths_by_index(os.listdir(restored_dir))
 
-    num_pairs = min(len(noisy_images), len(restored_images))
+    num_pairs = min(len(original_images), len(restored_images))
 
     ssim_value = 0
     psnr_value = 0
 
     for i in range(num_pairs):
-        noisy_image_path = os.path.join(noisy_dir, noisy_images[i])
+        i = 1
+        original_image_path = os.path.join(original_dir, original_images[i])
         restored_image_path = os.path.join(restored_dir, restored_images[i])
 
-        noisy_image = cv2.imread(noisy_image_path)
+        original_image = cv2.imread(original_image_path)
         restored_image = cv2.imread(restored_image_path)
 
-        psnr_value += psnr(restored_image, noisy_image)
-        ssim_value += ssim(restored_image, noisy_image, channel_axis=2)
+        psnr_value += psnr(restored_image, original_image)
+        ssim_value += ssim(restored_image, original_image, channel_axis=2)
 
     mean_computed_psnr = psnr_value / num_pairs
     mean_computed_ssim = ssim_value / num_pairs
 
+    print(psnr_value, ssim_value, num_pairs)
+
     return mean_computed_psnr, mean_computed_ssim
+
+
+def sort_paths_by_index(paths):
+    return sorted(paths, key=lambda x: int(x.split('_')[-1].split('.')[0]))
