7d094d1287f7db87faf0d6b311d25ef24331ed70
federico_calandra <federico12597@gmial.com>
2023-11-11 10:42:42 +0100

    Refactoring

--- dlproject/data/visualize.py
+++ dlproject/data/visualize.py
@@ -33,13 +33,13 @@ def show_latent_plane_sampled_points(decoder: tf.keras.Model,
     plt.imshow(figure)
 
 
-def display_random_images(test_data, noisy_test_data, trained_model, num_images=10):
+def display_random_images(test_data, noisy_test_data, predicted_output, num_images=10):
     # Randomly select 10 indices from the test dataset
     random_indices = random.sample(range(len(test_data)), num_images)
-    display_images(test_data, noisy_test_data, trained_model, random_indices, num_images)
+    display_images(test_data, noisy_test_data, predicted_output, random_indices, num_images)
 
 
-def display_images(test_data, noisy_test_data, trained_model, image_indexes, num_images):
+def display_images(test_data, noisy_test_data, predicted_output, image_indexes, num_images):
     # Randomly select 10 indices from the test dataset
     plt.figure(figsize=(15, 4))
 
@@ -55,9 +55,8 @@ def display_images(test_data, noisy_test_data, trained_model, image_indexes, num
         plt.axis('off')
 
         # Predicted output from the autoencoder
-        predicted_output = trained_model.predict(noisy_test_data[idx].reshape(1, 32, 32, 3))
         plt.subplot(3, num_images, 2 * num_images + i + 1)
-        plt.imshow(predicted_output[0])
+        plt.imshow(predicted_output[idx])
         plt.axis('off')
 
     plt.show()
